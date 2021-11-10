3a51f0b6f74e429bf6ce85872b8e40b44a35801a
pengfei <liu.pengfei@hotmail.fr>
2021-11-10 14:22:16 +0100

    change recursive to queue poping

---
+++ tests/s3_scanner_test.py
@@ -0,0 +1,10 @@
+from atlas_s3_hook.S3Scanner import S3Scanner
+
+
+#
+def test_is_ignored_positive():
+    assert S3Scanner.is_ignored(".keep")
+
+
+def test_is_ignored_negative():
+    assert not S3Scanner.is_ignored("keep")
